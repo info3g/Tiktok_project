3d373df6d5d8b3a20bf429e72c83def1db8a8e11
info3g <infothreeg@gmail.com>
2021-11-09 15:12:20 +0530

    updated code

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class AI_summery(models.Model):
+    total_calls= models.CharField(max_length=500,editable=True,null=True)
+    Successful_calls= models.CharField(max_length=500,editable=True,null=True)
+    Response_time= models.CharField(max_length=500,editable=True,null=True)
+    Timestamp= models.CharField(max_length=500,editable=True,null=True)
+
+
+class total_hits(models.Model):
+    idd = models.IntegerField(primary_key = True)
+    number_calls= models.CharField(max_length=500,editable=True,null=True)
+    Successful_calls= models.CharField(max_length=500,editable=True,null=True)
+def __str__(self):
+    return self.total_calls
--- home/views.py
+++ home/views.py
@@ -26,15 +26,13 @@ class UserInfoList(APIView):
 				print(users)
 				driver.get("https://www.tiktok.com/"+str(users))
 			driver.get("https://www.tiktok.com/@paaubookss")
-			time.sleep(10)
+			import  time
+			time.sleep(15)
 			bs_data = driver.page_source
 			soup = bs(bs_data, "html.parser")
-			time.sleep(10)
 			data_all = soup.find("script", attrs={"id": "__NEXT_DATA__"})
 			data=str(data_all).replace('<script crossorigin="anonymous" id="__NEXT_DATA__" nonce="" type="application/json">','').replace('</script>','')    
-			time.sleep(10)
 			import json
-			time.sleep(10)
 			res = json.loads(data)
 			print(res)
 			userInfo = res['props']['pageProps']['userInfo']
@@ -48,41 +46,37 @@ class UserVideo(APIView):
 		if serializer.is_valid():	
 			user = serializer.data['userid']
 			print(user)
-			count = serializer.data['userid']
+			countss = serializer.data['counts']
 			if '@' in user:
 				driver.get("https://www.tiktok.com/"+str(user))
 			else:
 				users = '@'+str(user)
 				driver.get("https://www.tiktok.com/"+str(users))
+			import  time
 			time.sleep(10)
 			bs_data = driver.page_source
 			soup = bs(bs_data, "html.parser")
-			time.sleep(10)
 			data_all = soup.find("script", attrs={"id": "__NEXT_DATA__"})
 			data=str(data_all).replace('<script crossorigin="anonymous" id="__NEXT_DATA__" nonce="" type="application/json">','').replace('</script>','')    
-			time.sleep(10)
 			import json
-			time.sleep(10)
 			res = json.loads(data)
 			itemList = res['props']['pageProps']['items']
-			print({'itemList':itemList})
+			# print({'itemList':itemList})
 			import time
 			page_source = []
+			time.sleep(20)
 			data=driver.find_elements_by_xpath('//*[@class="jsx-1906445185 video-feed-item-wrapper"]')
+			print(data,"dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
 			for i in data:
 				driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 				time.sleep(3)
 				links = i.get_attribute('href')
-				if count <= 10:
-					page_source.append(links)       
+				print(links,"llllllllllllllllllllllllllllllllllllllllllllllllllllllllllll")
+				if len(page_source) <= int(countss):
+					page_source.append(links)
 				else:
-					if len(page_source) == 20:
-						page_source.append(links)
-						print("Loop Break")
-						break
-				if len(page_source) == 20:
-					print("Loop Break")
-					break
-			return Response({'itemList':itemList,'page_source':page_source})
+					break       
+				
+			return Response({'itemList':itemList,'images':page_source})
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
